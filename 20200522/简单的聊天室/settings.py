a27f1b3d1d9caa1d4f517f513c5af32d0a831bb3
Aimee888 <hbut1210200508@sina.com>
2020-05-22 23:12:25 +0800

    chat room not successful at windows

---
+++ 20200522/简单的聊天室/settings.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@Project -> File   ：six-dialog_design -> settings
+@IDE    ：PyCharm
+@Author ：zihan
+@Date   ：2020/5/22 22:19
+@Desc   ：https://www.cnblogs.com/noonjuan/p/11295438.html
+@version : 0.0.0.1 --》 2020/05/22
+=================================================="""
+# settings.py
+
+HOST = '127.0.0.1'      # 主机名
+PORT = 5555            # 端口号
+buffersize = 1024    # 缓冲大小
+ADDR = HOST, PORT    # 地址
+
+languages = ['cn', 'en']      # 'cn' -> 中文
+language = 'cn'                # 'en' -> 英文
+
+curuser = 'a'  # 当前用户
+
+
